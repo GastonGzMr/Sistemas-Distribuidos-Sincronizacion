a5f7c4f59a6ba15dd5abe135a1f249784cfc2b3e
Gastón Gomariz Moser <54191173+GastonGzMr@users.noreply.github.com>
2020-10-14 22:01:46 -0300

    v1
    Agregadas las clases compartidas

---
+++ CristianSyncAlgorithm.java
@@ -0,0 +1,13 @@
+package project;
+import java.util.ArrayList;
+
+class CristianSyncAlgorithm implements SyncAlgorithm {
+
+    public void CristianSyncAlgorithm(){
+        
+    } 
+    
+    public void Syncronized(ArrayList<Machine> listamaquinas){
+        
+    }
+}
---
+++ SyncAlgorithm.java
@@ -0,0 +1,8 @@
+package project;
+import java.util.ArrayList;
+
+interface SyncAlgorithm {
+    
+   public void Syncronized(ArrayList<Machine> listamaquinas);
+    
+}
